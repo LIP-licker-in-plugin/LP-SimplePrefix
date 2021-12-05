a7e04e998ac2e4cd5541752412173f68d4dcb298
darksoldier1404 <darksoldier1404@gmail.com>
2021-12-05 15:53:15 +0900

    1.12, 1.13, 1.14, 1.15, 1.16 모듈 추가, 1.12~1.13 AnvilGUI 로직 제거, 칭호 설정 커맨드 레거시 방식으로 변경

---
+++ v1_16_5/src/main/java/com/darksoldier1404/dsp/events/DSPEvent.java
@@ -0,0 +1,81 @@
+package com.darksoldier1404.dsp.events;
+
+import com.darksoldier1404.dsp.SimplePrefix;
+import com.darksoldier1404.dsp.functions.DSPFunction;
+import com.darksoldier1404.duc.utils.ConfigUtils;
+import com.darksoldier1404.duc.utils.NBT;
+import org.bukkit.ChatColor;
+import org.bukkit.configuration.file.YamlConfiguration;
+import org.bukkit.entity.Player;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.Listener;
+import org.bukkit.event.inventory.InventoryCloseEvent;
+import org.bukkit.event.inventory.InventoryType;
+import org.bukkit.event.player.PlayerChatEvent;
+import org.bukkit.event.player.PlayerInteractEvent;
+import org.bukkit.event.player.PlayerJoinEvent;
+import org.bukkit.event.player.PlayerQuitEvent;
+import org.bukkit.inventory.AnvilInventory;
+import org.bukkit.inventory.EquipmentSlot;
+import org.bukkit.inventory.ItemStack;
+
+import static org.bukkit.event.block.Action.RIGHT_CLICK_AIR;
+import static org.bukkit.event.block.Action.RIGHT_CLICK_BLOCK;
+
+@SuppressWarnings("all")
+public class DSPEvent implements Listener {
+    private final SimplePrefix plugin = SimplePrefix.getInstance();
+
+    @EventHandler
+    public void onJoin(PlayerJoinEvent e) {
+        plugin.udata.put(e.getPlayer().getUniqueId(), ConfigUtils.initUserData(plugin, e.getPlayer().getUniqueId().toString(), "users", plugin.defaultData));
+        ConfigUtils.saveCustomData(plugin, plugin.udata.get(e.getPlayer().getUniqueId()), e.getPlayer().getUniqueId().toString(), "users");
+    }
+
+    @EventHandler
+    public void onQuit(PlayerQuitEvent e) {
+        YamlConfiguration data = plugin.udata.get(e.getPlayer().getUniqueId());
+        ConfigUtils.saveCustomData(plugin, data, e.getPlayer().getUniqueId().toString(), "users");
+        plugin.udata.remove(e.getPlayer().getUniqueId());
+    }
+
+    @EventHandler
+    public void onInventoryClose(InventoryCloseEvent e) {
+        if (e.getView().getType() == InventoryType.ANVIL) {
+            AnvilInventory inv = (AnvilInventory) e.getInventory();
+            System.out.println(inv.getRenameText());
+        }
+    }
+
+    @EventHandler
+    public void onChat(PlayerChatEvent e) {
+        Player p = e.getPlayer();
+        if (!(plugin.udata.get(p.getUniqueId()).getString("Player.Prefix") == null)) {
+            String name = plugin.udata.get(p.getUniqueId()).getString("Player.Prefix") == null ? "" : plugin.udata.get(p.getUniqueId()).getString("Player.Prefix");
+            plugin.config.getConfigurationSection("Settings.PrefixList").getKeys(false).forEach(s -> {
+                if (s.equals(name)) {
+                    e.setFormat(ChatColor.translateAlternateColorCodes('&', plugin.config.getString("Settings.PrefixList." + name)) + e.getFormat());
+                }
+            });
+        }
+    }
+
+    @EventHandler
+    public void onInteract(PlayerInteractEvent e) {
+        if (e.getAction() != RIGHT_CLICK_AIR || e.getAction() != RIGHT_CLICK_BLOCK) return;
+        if (e.getHand() == EquipmentSlot.OFF_HAND) return;
+        if (e.getItem() == null) return;
+        ItemStack item = e.getItem();
+        if (!item.hasItemMeta()) return;
+        if (NBT.hasTagKey(item, "dsp.prefix")) {
+            String name = NBT.getStringTag(item, "dsp.prefix");
+            plugin.config.getConfigurationSection("Settings.PrefixList").getKeys(false).forEach(s -> {
+                if (s.equals(name)) {
+                    if (DSPFunction.givePrefix(e.getPlayer(), name)) {
+                        item.setAmount(item.getAmount() - 1);
+                    }
+                }
+            });
+        }
+    }
+}
